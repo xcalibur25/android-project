87f307f8885a53bd56cffee83f186dbe4f7cd6e0
Yash <yashraorane25@gmail.com>
2019-02-04 15:42:03 +0530

    04/02/2019

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view){
+
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Log.i("information",myTextField.getText().toString());
+    }
+}
